9879c313d8201c73d2a4bbc9dd8bf609926073ef
Arturo Perez <dash_arturo@hotmail.com>
2023-03-03 15:01:49 -0600

    Eventos -> Validar si hay salas disponibles y si hay sala disponible Validar si hay rango de fecha disponible

--- app/Http/Controllers/Admin/EventosController.php
+++ app/Http/Controllers/Admin/EventosController.php
@@ -7,11 +7,12 @@ use App\Models\Models\Evento;
 use App\Models\Models\Sala;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\Rule;
 
 class EventosController extends Controller
 {
-    public function  __construct()
+    public function __construct()
     {
         $this->middleware('auth');//
     }
@@ -20,13 +21,14 @@ class EventosController extends Controller
     {
         $salas = Sala::all();
         $users = User::all();
-        $eventos  = Evento::all();
-        return view('admin.eventos.index',[
-            'eventos'=> $eventos,
+        $eventos = Evento::all();
+        return view('admin.eventos.index', [
+            'eventos' => $eventos,
             'salas' => $salas,
             'users' => $users
         ]);
     }
+
     public function store(Request $request)
     {
         $request->validate([
@@ -37,48 +39,44 @@ class EventosController extends Controller
         ]);
 
 
-
         $newEvento = new Evento();
 
 
-        $newEvento->user_id = $request-> user_id;
-        $sala_id = $newEvento->salas_id = $request-> salas_id;
-        $newEvento->fecha_entrada = $request-> fecha_entrada;
-        $newEvento->fecha_salida = $request-> fecha_salida;
-        $newEvento->email_solicitante = $request-> email_solicitante;
+        $newEvento->user_id = $request->user_id;
+        $sala_id = $newEvento->salas_id = $request->salas_id;
+        $fecha_inicio = $newEvento->fecha_entrada = $request->fecha_entrada;
+        $fecha_fin = $newEvento->fecha_salida = $request->fecha_salida;
+        $newEvento->email_solicitante = $request->email_solicitante;
 
-        $validacion = validator($newEvento->toArray(),[
-            'salas_id' => 'required|integer',
-            'fecha_entrada'=> [
-                'required',
-                Rule::unique('eventos','fecha_entrada')->where(function ($request) use ($sala_id){
-                    return $request->where('salas_id',$sala_id);
-                })
-            ],
-            'fecha_salida' => 'required|date|after:fecha_entrada',
-        ]);
-
-        if($validacion->fails()){
-            print ('La fecha ya existe para ese id');
-        } else{
+        //Registrar si una sala esta disponible dentro del rango de las fechas
+        $validacionRangoFechas = DB::table('eventos')
+            ->where('salas_id', $sala_id)
+            ->where(function ($request) use ($fecha_inicio, $fecha_fin) {
+                $request->whereBetween('fecha_entrada', [$fecha_inicio, $fecha_fin])
+                    ->orWhereBetween('fecha_salida', [$fecha_inicio, $fecha_fin]);
+            })->get();
+        if ($validacionRangoFechas->isEmpty()) {
             $newEvento->save();
-        }
+            return redirect()->back();
+        } else {
 
-        return redirect()->back();
+            return redirect()->back();
+        }
     }
+
 //
-    public function update(Request $request,$eventoId)
+    public function update(Request $request, $eventoId)
     {
         $evento = (Evento::find($eventoId));
 
-        $evento->fecha_entrada = $request-> fecha_entrada;
-        $evento->fecha_salida = $request-> fecha_salida;
+        $evento->fecha_entrada = $request->fecha_entrada;
+        $evento->fecha_salida = $request->fecha_salida;
         $evento->save();
 
         return redirect()->back();
     }
 
-    public function delete(Request $request,$eventoId)
+    public function delete(Request $request, $eventoId)
     {
         $evento = (Evento::find($eventoId));
         $evento->delete();
